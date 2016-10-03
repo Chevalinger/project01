8de5dcfb3c4467b60bd8a697407e29c9077cefc4
Chadrick Frederick <cfreder6@u.rochester.edu>
2016-10-03 03:24:57 -0400

    Added content files via upload

---
+++ project1/colophon.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title> CSC 174 Project 1</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+
+	<div class="container">
+	<header>
+		<h1>
+			Sports!
+		</h1>
+
+	</header>
+
+      <?php include "inc/nav.inc"; ?>
+
+	<header>
+		<h2>Highlights of the Project</h2>
+	</header>
+
+		<section>
+			<article>
+				<p>Contented will added soon</p>
+		
+
+					<h3>Member Tasks </h3>
+						<p>Contented will added soon</p>
+
+					<h3>Technologies Used</h3>
+						<p>Contented will added soon</p>
+		
+					<h3>Content Sources</h3>
+						<p>Contented will added soon</p>
+
+		
+			</article>
+		</section>
+
+			<footer>
+				<h3>Team 5, CSC	174	Advance Front-End Web Development</h3>	
+			</footer>
+
+	</div><!-- .container -->
+
+		<!-- Site information was collected from wikipedia at https://en.wikipedia.org/-->
+
+	</body>
+</html>
---
+++ project1/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title> CSC 174 Project 1</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+
+	<div class="container">
+	<header>
+		<h1>
+			Sports!
+		</h1>
+
+	</header>
+
+     <?php include "inc/nav.inc"; ?>
+
+	<header>
+		<h2>Highlights of the Sport</h2>
+	</header>
+
+		<section>
+			<article>
+					<p>Sport (UK) or sports (US) are all forms of usually competitive physical activity or games which,[1] through casual or organised participation, aim to use, maintain or improve physical ability and skills while providing enjoyment to participants, and in some cases, entertainment for spectators.[2] Usually the contest or game is between two sides, each attempting to exceed the other. Some sports allow a tie game; others provide tie-breaking methods, to ensure one winner and one loser. A number of such two-sided contests may be arranged in a tournament producing a champion. Many sports leagues make an annual champion by arranging games in a regular sports season, followed in some cases by playoffs. Hundreds of sports exist, from those between single contestants, through to those with hundreds of simultaneous participants, either in teams or competing as individuals. In certain sports such as racing, many contestants may compete, each against all with one winner Please read short excerpts about a few of these sports below.</p>
+		
+
+					<h3>Soccer</h3>
+						<p>Association football, more commonly known as football or soccer,[3] nicknamed The World Game or The Beautiful Game, is a team sport played between two teams of eleven players with a spherical ball. It is played by 250 million players in over 200 countries and dependencies, making it the world's most popular sport.[4][5][6][7] The game is played on a rectangular field with a goal at each end. The object of the game is to score by getting the ball into the opposing goal.</p>
+
+						<p><a href="soccer.php">Click here to read more about Soccer.</a></p>
+
+					<h3>Frisbee</h3>
+						<p>A frisbee (sometimes called a flying disc) is a disc-shaped gliding toy or sporting item that is generally plastic and roughly 20 to 25 centimetres (8 to 10 in) in diameter with a lip,[1] used recreationally and competitively for throwing and catching, for example, in flying disc games. The shape of the disc, an airfoil in cross-section, allows it to fly by generating lift as it moves through the air while spinning.</p>
+
+						<p><a href="frisbee.php">Click here to read more about Frisbee.</a></p>
+		
+					<h3>Baseball</h3>
+						<p>Baseball is a bat-and-ball game played between two teams of nine players each, who take turns batting and fielding.
+						The batting team attempts to score runs by hitting a ball that is thrown by the pitcher with a bat swung by the batter, then running counter-clockwise around a series of four bases: first, second, third, and home plate. A run is scored when a player advances around the bases and returns to home plate.</p>
+
+						<p><a href="baseball.php">Click here to read more about Baseball.</a></p>
+		
+			</article>
+		</section>
+
+			<footer>
+				<h3>Team 5, CSC	174	Advance Front-End Web Development</h3>	
+			</footer>
+
+	</div><!-- .container -->
+
+		<!-- Site information was collected from wikipedia at https://en.wikipedia.org/wiki/Sport-->
+
+	</body>
+</html>
