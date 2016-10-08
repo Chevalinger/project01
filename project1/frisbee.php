45cae91acfd1714d04d5f4cf5d9c74c621e2bacb
Mariana <mfloresk@u.rochester.edu>
2016-10-07 22:50:49 -0400

    Same as in baseball.php

--- project1/frisbee.php
+++ project1/frisbee.php
@@ -1,101 +1,92 @@
-<!DOCTYPE html> 
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Frisbee, CSC 174 Project 1</title>
-        <link rel="stylesheet" href="css/styles.css">
-    </head>
-    <body>
-      <div class="container">
-      <header>
-        <h1>
-          Sports!
-        </h1>
+	<?php 
+	  $currentPage = "Home";	
+	  include "inc/topPart.inc"; 
+	  ?>
 
-      </header>
+	<?php include "inc/nav.inc"; ?>
 
-      <?php include "inc/nav.inc"; ?>
+	<header>
+		<h2>Highlights of the Sport</h2>
+	</header>
 
-        <header>
-            <h2>Frisbee</h2>
-        </header>
+		<section>
+			<article>
+					<p>Sport (UK) or sports (US) are all forms of usually competitive physical activity or games which,[1] through casual or organised participation, aim to use, maintain or improve physical ability and skills while providing enjoyment to participants, and in some cases, entertainment for spectators.[2] Usually the contest or game is between two sides, each attempting to exceed the other. Some sports allow a tie game; others provide tie-breaking methods, to ensure one winner and one loser. A number of such two-sided contests may be arranged in a tournament producing a champion. Many sports leagues make an annual champion by arranging games in a regular sports season, followed in some cases by playoffs. Hundreds of sports exist, from those between single contestants, through to those with hundreds of simultaneous participants, either in teams or competing as individuals. In certain sports such as racing, many contestants may compete, each against all with one winner Please read short excerpts about a few of these sports below.</p>
+		
 
-        <section>
-          <article>
-              <h3>Brief</h3>
-                <p>A frisbee (sometimes called a flying disc) is a disc-shaped gliding toy or sporting item that is generally plastic and roughly 20 to 25 centimetres (8 to 10 in) in diameter with a lip,[1] used recreationally and competitively for throwing and catching, for example, in flying disc games. The shape of the disc, an airfoil in cross-section, allows it to fly by generating lift as it moves through the air while spinning.</p>
+					<h3>Soccer</h3>
+						<p>Association football, more commonly known as football or soccer,[3] nicknamed The World Game or The Beautiful Game, is a team sport played between two teams of eleven players with a spherical ball. It is played by 250 million players in over 200 countries and dependencies, making it the world's most popular sport.[4][5][6][7] The game is played on a rectangular field with a goal at each end. The object of the game is to score by getting the ball into the opposing goal.</p>
 
-				<p>Frisbees are thrown and caught for free-form (freestyle) recreation and as part of many flying disc games. A wide range of frisbees variants are available commercially. Disc golf discs are usually smaller but denser and tailored for particular flight profiles to increase/decrease stability and distance. The longest recorded disc throw is by Simon Lizotte with a distance of 263.2 meters.[2] Disc dog sports use relatively slow flying discs made of more pliable material to better resist a dog's bite and prevent injury to the dog. Flying rings are also available; they typically travel significantly farther than any traditional flying disc. There are also illuminated discs meant for nighttime play — they are made of a phosphorescent plastic or contain battery-powered light-emitting diodes or chemiluminescent glowing sticks. Others whistle when they reach a certain velocity in flight.</p>
+						<p><a href="soccer.php" class="waves-effect waves-light btn">Click here to read more about Soccer</a></p>
 
-                <p>Lift is generated in the same way as a traditional airfoil. The rotating frisbee has a nearly vertical angular momentum vector, stabilizing its angle of attack via gyroscopic action. If the disc were not spinning, it would crash to pitch. When the disc is spinning, however, aerodynamic torque instead leads to precess about the spin axis, causing its trajectory to curve to the left or the right. Most discs are designed to be aerodynamically stable so that this roll is accurate for a fairly broad range of velocities and rates of spin. Many disc golf discs, however, are intentionally designed to be unstable. Higher rates of spin lead to more stability, and, for a given rate of spin, there is generally a range of velocities that are stable. Even a slight deformation in a disc (called a "taco," which in extreme cases looks like a taco shell) can cause negative effects when throwing long range. A disk can be checked for these deformations by holding it horizontally at eye level and looking at the rim while slowly turning it.[3]</p>
+					<h3>Frisbee</h3>
+						<p>A frisbee (sometimes called a flying disc) is a disc-shaped gliding toy or sporting item that is generally plastic and roughly 20 to 25 centimetres (8 to 10 in) in diameter with a lip,[1] used recreationally and competitively for throwing and catching, for example, in flying disc games. The shape of the disc, an airfoil in cross-section, allows it to fly by generating lift as it moves through the air while spinning.</p>
 
-              <h3>History</h3>
-                 <div>
-                  <figure>
-                    <img src="images/frisbee1.jpg" alt="Frisbee History">
-                    <figcaption>The first Frisbee (Professional Model) ever designed with a tournament identification. The 1972 Canadian Open Frisbee Championships,Toronto.</figcaption>
-                  </figure>
-                </div>
+						<p><a href="frisbee.php" class="waves-effect waves-light btn">Click here to read more about Frisbee.</a></p>
+		
+					<h3>Baseball</h3>
+						<p>Baseball is a bat-and-ball game played between two teams of nine players each, who take turns batting and fielding.
+						The batting team attempts to score runs by hitting a ball that is thrown by the pitcher with a bat swung by the batter, then running counter-clockwise around a series of four bases: first, second, third, and home plate. A run is scored when a player advances around the bases and returns to home plate.</p>
 
-                <p>Fred Morrison discovered a market for the modern-day flying disc[6] in 1938 when he and future wife, Lucile, were offered 25 cents for a cake pan that they were tossing back and forth on a beach in New Haven, CT. "That got the wheels turning, because you could buy a cake pan for five cents, and if people on the beach were willing to pay a quarter for it, well - there was a business," Morrison told The Virginian-Pilot newspaper in 2007.[7]</p>
+						<p><a href="baseball.php" class="waves-effect waves-light btn">Click here to read more about Baseball.</a></p>
+		
+			</article>
+		</section>
 
-				<p>The Morrisons continued their business until World War II, when Morrison served in the Army Air Force, flying P-47s, and then was a prisoner of war. Mustered out, Morrison sketched a design for an aerodynamically improved flying disc that he called the Whirlo-Way. By 1948, after design modifications and experimentation with several prototypes, Morrison and business partner Warren Franscioni began producing the first plastic discs, renaming them the Flyin-Saucer in the wake of reported unidentified flying object-sightings.</p>
+		<?php
+			$contentFrom = "https://en.wikipedia.org";
+			include "inc/footer.inc"
+		?>
 
-				<p>"We worked fairs, demonstrating it," Morrison told the Virginian-Pilot. The two of them once overheard someone saying the pair were using wires to make the discs hover, so they developed a sales pitch: "The Flyin-Saucer is free, but the invisible wire is $1." "That's where we learned we could sell these things," he said, because people were enthusiastic about them.</p>
+	</div><!-- .container -->
 
-				<p>Morrison and Franscioni ended their partnership in early 1950, and in 1954 Morrison formed his own company, called American Trends, to buy and sell Flyin-Saucers, which were by then being made of a flexible polypropylene plastic from Southern Connecticut Plastics, the original molder. After learning that he could produce his own disc more cheaply, in 1955 Morrison designed a new model, the Pluto Platter, the archetype of all modern flying discs. He sold the rights to Wham-O on January 23, 1957, and in 1958 Morrison was awarded U.S. Design Patent D183,626 for his product.</p>
+		<!-- Site information was collected from wikipedia at https://en.wikipedia.org/wiki/Sport-->
 
-                <p>In June 1957, Wham-O co-founder Richard Knerr decided to stimulate sales by giving the discs the additional brand name Frisbee (pronounced "friz'-bee"), after learning that Northeastern college students were calling the Pluto Platter by that name,[8] the term "Frisbee" coming from the name of the Connecticut-based pie manufacturer, Frisbie Pie Company,[9] which supplied pies to Yale University, where students started the campus craze by tossing empty pie tins stamped with the company's logo. "I thought the name was a horror. Terrible," Morrison told The Press-Enterprise of Riverside in 2007.[10] In 1982, Morrison told Forbes magazine that he had received about US$2 million in royalty payments and said: "I wouldn't change the name of it for the world."[10]</p>
+	</body>
+</html>
+	<?php 
+	  $currentPage = "Home";	
+	  include "inc/topPart.inc"; 
+	  ?>
 
+	<?php include "inc/nav.inc"; ?>
 
-              <h3>Games</h3>
-                <p>A scene from the 1956 film Mohawk, a remake of Drums along the Mohawk (1939), features the Iroquois girls playing with an object recognisable as a frisbee. The game of guts was invented by the Healy Brothers in the 1950s and developed at the International Frisbee Tournament (IFT) in Eagle Harbor, Michigan. The game of ultimate, the most widely played disc game, began in the late 1960s with Joel Silver and Jared Kass. In the 1970s it developed as an organized sport with the creation of the Ultimate Players Association with Dan Roddick, Tom Kennedy and Irv Kalb. Double disc court was invented and introduced in the early 1970s by Jim Palmeri. In 1974, freestyle competition was created and introduced by Ken Westerfield and Discrafts Jim Kenner.[19] Freestyle judging standards were then developed by Jim Palmeri and the Freestyle Players Association led by Dave Marini. In 1976, the game of disc golf was standardized with targets called "pole holes" invented and developed by Wham-O's Ed Headrick.</p>
-          </article>
+	<header>
+		<h2>Highlights of the Sport</h2>
+	</header>
 
-          <aside>
-            <h3>Frisbee</h3>
-            <div>
-              <figure>
-                <img src="images/frisbee2.jpg" alt="Frisbee Throw">
-                <figcaption>Someone throwing a flying disc</figcaption>
-              </figure>
-            </div>
+		<section>
+			<article>
+					<p>Sport (UK) or sports (US) are all forms of usually competitive physical activity or games which,[1] through casual or organised participation, aim to use, maintain or improve physical ability and skills while providing enjoyment to participants, and in some cases, entertainment for spectators.[2] Usually the contest or game is between two sides, each attempting to exceed the other. Some sports allow a tie game; others provide tie-breaking methods, to ensure one winner and one loser. A number of such two-sided contests may be arranged in a tournament producing a champion. Many sports leagues make an annual champion by arranging games in a regular sports season, followed in some cases by playoffs. Hundreds of sports exist, from those between single contestants, through to those with hundreds of simultaneous participants, either in teams or competing as individuals. In certain sports such as racing, many contestants may compete, each against all with one winner Please read short excerpts about a few of these sports below.</p>
+		
 
-            <div>
-              <figure>
-                <img src="images/frisbee3.jpg" alt="Frisbee Catch">
-                <figcaption>Person catching a flying disc</figcaption>
-              </figure>
-            </div>
+					<h3>Soccer</h3>
+						<p>Association football, more commonly known as football or soccer,[3] nicknamed The World Game or The Beautiful Game, is a team sport played between two teams of eleven players with a spherical ball. It is played by 250 million players in over 200 countries and dependencies, making it the world's most popular sport.[4][5][6][7] The game is played on a rectangular field with a goal at each end. The object of the game is to score by getting the ball into the opposing goal.</p>
 
-              <h4>Related games</h4>
-                <ul>
-                  <li>Crosbee</li>
-                  <li>Disc dog</li>
-                  <li>Disc golf</li>
-                  <li>Dodge disc</li>
-                  <li>Double disc court</li>
-                  <li>Durango boot</li>
-                  <li>Death Disc</li>
-                  <li>Ultimate Ascent</li>
-                  <li>Wilderness Frisbee®</li>
-                  <li>Fricket</li>
-                  <li>Kan-jam</li>
-                  <li>Goaltimate</li>
-                </ul>
+						<p><a href="soccer.php" class="waves-effect waves-light btn">Click here to read more about Soccer</a></p>
 
-          </aside>
-        </section>
+					<h3>Frisbee</h3>
+						<p>A frisbee (sometimes called a flying disc) is a disc-shaped gliding toy or sporting item that is generally plastic and roughly 20 to 25 centimetres (8 to 10 in) in diameter with a lip,[1] used recreationally and competitively for throwing and catching, for example, in flying disc games. The shape of the disc, an airfoil in cross-section, allows it to fly by generating lift as it moves through the air while spinning.</p>
 
-          <footer>
-          <h3>Team 5, CSC 174 Advance Front-End Web Development</h3>
-            <h4>Citations</h4>
+						<p><a href="frisbee.php" class="waves-effect waves-light btn">Click here to read more about Frisbee.</a></p>
+		
+					<h3>Baseball</h3>
+						<p>Baseball is a bat-and-ball game played between two teams of nine players each, who take turns batting and fielding.
+						The batting team attempts to score runs by hitting a ball that is thrown by the pitcher with a bat swung by the batter, then running counter-clockwise around a series of four bases: first, second, third, and home plate. A run is scored when a player advances around the bases and returns to home plate.</p>
 
-            <ul>
-              <li>Content comes from <a href="https://en.wikipedia.org/wiki/Frisbee">Wikipedia/frisbee,</a></li>
-            </ul>
-          </footer>
-      </div><!-- .container -->
-    </body>
+						<p><a href="baseball.php" class="waves-effect waves-light btn">Click here to read more about Baseball.</a></p>
+		
+			</article>
+		</section>
+
+		<?php
+			$contentFrom = "https://en.wikipedia.org";
+			include "inc/footer.inc"
+		?>
+
+	</div><!-- .container -->
+
+		<!-- Site information was collected from wikipedia at https://en.wikipedia.org/wiki/Sport-->
+
+	</body>
 </html>
