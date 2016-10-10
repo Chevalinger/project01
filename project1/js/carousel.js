5a102d2a932a2b7552ad7f7eacc050e70bdf2fb4
JacobMargolis <jmargol4@u.rochester.edu>
2016-10-09 21:36:53 -0400

    moved carousel to just index
    can someone else try looking at the page on both firefox and chrome? It
    seems to only work on firefox for me, but I'm not sure if thats just a
    caching issue on my end

--- project1/js/carousel.js
+++ project1/js/carousel.js
@@ -1,5 +1,10 @@
 $(window).load(function(){
       $('.carousel').carousel();
+      var intervalID = setInterval(function() {
+		$('.carousel').carousel('next');
+	}, 5000);
 });
 
 $('.carousel.carousel-slider').carousel({full_width: true});
+
+
