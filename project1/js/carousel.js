45f03b0a0a8f4b8e840ebfdd334cf57f0460b4df
JacobMargolis <jmargol4@u.rochester.edu>
2016-10-10 19:10:28 -0400

    Added js comments, to do list

--- project1/js/carousel.js
+++ project1/js/carousel.js
@@ -1,10 +1,14 @@
 $(window).load(function(){
-      $('.carousel').carousel();
-      var intervalID = setInterval(function() {
-		$('.carousel').carousel('next');
-	}, 5000);
+		// Activate the carousel
+      	$('.carousel').carousel();
+      	// Change slides every 5 seconds
+      	var intervalID = setInterval(function() {
+			$('.carousel').carousel('next');
+		}, 5000);
+
 });
 
+//Display carousel across the length of the screen
 $('.carousel.carousel-slider').carousel({full_width: true});
 
 
