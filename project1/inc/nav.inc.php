41d52e6b294e7695265f7b828fc3902d37c7f92a
marianafk <mfloresk@u.rochester.edu>
2016-10-10 22:13:23 -0400

    Added some styles
    Changed hover characteristics of nav elements.
    -Changed size and color of elements
    -Made the home button bold and bigger

--- project1/inc/nav.inc.php
+++ project1/inc/nav.inc.php
@@ -4,18 +4,18 @@
       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">reorder</i></a>
       <ul id="nav-mobile" class="left hide-on-med-and-down">
           <li class="home-big" <?php if ($currentPage == "Home") echo 'class="active"';?>><a href="index.php">Home</a></li>
-          <li <?php if ($currentPage == "Soccer") echo 'class="active"';?>><a href="soccer.php">Soccer</a></li>
-          <li <?php if ($currentPage == "Frisbee") echo 'class="active"';?>><a href="frisbee.php">Frisbee</a></li>
-          <li <?php if ($currentPage == "Baseball") echo 'class="active"';?>><a href="baseball.php">Baseball</a></li>
-          <li <?php if ($currentPage == "Colophon") echo 'class="active"';?>><a href="colophon.php">Colophon</a></li>
+          <li class= "nav-element" <?php if ($currentPage == "Soccer") echo 'class="active"';?>><a href="soccer.php">Soccer</a></li>
+          <li class= "nav-element" <?php if ($currentPage == "Frisbee") echo 'class="active"';?>><a href="frisbee.php">Frisbee</a></li>
+          <li class= "nav-element" <?php if ($currentPage == "Baseball") echo 'class="active"';?>><a href="baseball.php">Baseball</a></li>
+          <li class= "nav-element" <?php if ($currentPage == "Colophon") echo 'class="active"';?>><a href="colophon.php">Colophon</a></li>
       </ul>
 
        <ul class="side-nav indigo lighten-4" id="mobile-demo">
         <li <?php if ($currentPage == "Home") echo 'class="active"';?>><a href="index.php">Home</a></li>
-          <li <?php if ($currentPage == "Soccer") echo 'class="active"';?>><a href="soccer.php">Soccer</a></li>
-          <li <?php if ($currentPage == "Frisbee") echo 'class="active"';?>><a href="frisbee.php">Frisbee</a></li>
-          <li <?php if ($currentPage == "Baseball") echo 'class="active"';?>><a href="baseball.php">Baseball</a></li>
-          <li <?php if ($currentPage == "Colophon") echo 'class="active"';?>><a href="colophon.php">Colophon</a></li>
+          <li class= "nav-element" <?php if ($currentPage == "Soccer") echo 'class="active"';?>><a href="soccer.php">Soccer</a></li>
+          <li class= "nav-element" <?php if ($currentPage == "Frisbee") echo 'class="active"';?>><a href="frisbee.php">Frisbee</a></li>
+          <li class= "nav-element" <?php if ($currentPage == "Baseball") echo 'class="active"';?>><a href="baseball.php">Baseball</a></li>
+          <li class= "nav-element" <?php if ($currentPage == "Colophon") echo 'class="active"';?>><a href="colophon.php">Colophon</a></li>
       </ul>
       </ul>
     </div>
