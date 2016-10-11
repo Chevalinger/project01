ca6d077d9d9ce0259a63659c680f27a3ee58acaf
marianafk <mfloresk@u.rochester.edu>
2016-10-11 17:12:34 -0400

    This is what didn't register for the Colophon.
    I worked on this some hours ago, but Github sent an error. If you still
    want to use it, go ahead. :)

--- project01/colophon.php
+++ project01/colophon.php
@@ -19,43 +19,69 @@
 					<h3>Member Tasks</h3>
 						<ul class="collection tasks">
 						    <li class="collection-item avatar indigo lighten-3">
-						      <i class="material-icons circle blue">store</i>
-						      <span class="title">Alizah Barker</span>
-						      <p>Architect<br>
-						         Design elements, Mobile friendliness, Framework elements, General help
-						      </p>
+						     <i class="material-icons circle blue">store</i>
+						     <span class="title">Alizah Barker</span>
+						     <h5>Architect</h5>
+						     <ul class = "mem-tasks">
+						      	<li>Chose Framework</li>
+						      	<li>Created Repository</li>
+						      	<li>Design Elements</li>
+						      	<li>Mobile Friendliness</li>
+						      	<li>Framework Elements</li>
+						      	<li>General Help</li>
+						    </ul>
+
 						      
 						    </li>
 						    <li class="collection-item avatar indigo lighten-3">
 						      <i class="material-icons circle orange">http</i>
 						      <span class="title">Chadrick Frederick</span>
-						      <p>Content Master and Designer<br>
-						         Content topic, Content structure
-						      </p>
+						    <h5>Content Master and Designer</h5>
+						    <ul class = "mem-tasks">
+						      	<li>Chose Topic: Sports</li>
+						      	<li>Structured Content</li>
+						      	<li>Sections organization</li>
+						      	<li>PHP Includes</li>
+						    </ul>
 						     
 						    </li>
 						    <li class="collection-item avatar indigo lighten-3">
 						      <i class="material-icons circle green">mode_edit</i>
 						      <span class="title">Daniel Gomez</span>
-						      <p>CSS Artist<br>
-						         CSS customization
-						      </p>
+						    <h5>CSS Artist</h5>
+						    <ul class = "mem-tasks">
+						      	<li>Overrode Materialize</li>
+						      	<li>Selected Elements to Edit</li>
+						      	<li>Customed Website</li>
+						      	<li>Organization of CSS File</li>
+						    </ul>
 						      
 						    </li>
 						    <li class="collection-item avatar indigo lighten-3">
 						      <i class="material-icons circle red">view_quilt</i>
 						      <span class="title">Mariana Flores Kim</span>
-						      <p>Framework Wrangler<br>
-						         Framework, Code cleaning, design
-						      </p>
+						    <h5>Framework Wrangler</h5>
+						    <ul class = "mem-tasks">
+						      	<li>Created New Classes</li>
+						      	<li>Edited and Cleaned Code</li>
+						      	<li>Applied Decisions</li>
+						      	<li>Navigation Bar Extra Effects</li>
+						      	<li>Footer Look</li>
+						      	<li>Edited Colophon</li>
+						    </ul>
 						      
 						    </li>
 						    <li class="collection-item avatar indigo lighten-3">
-						      <i class="material-icons circle purple">view_carousel</i>
-						      <span class="title">Jacob Margolis</span>
-						      <p>JavaScript Coder<br>
-						      	Carousel, SideNav, General help
-						      </p>
+						    <i class="material-icons circle purple">view_carousel</i>
+						    <span class="title">Jacob Margolis</span>
+						    <h5>JavaScript Coder</h5>
+						    <ul class = "mem-tasks">
+						      	<li>Designed Carousel</li>
+						      	<li>Mobile Compatibility</li>
+						      	<li>Other Functionalities</li>
+						      	<li>To-do List for Team</li>
+						      	<li>General Help</li>
+						    </ul>
 						      
 						    </li>
 						 </ul>
@@ -72,7 +98,8 @@
 							</ul>
 		
 					<h3>Content Sources</h3>
-						<p>Wikipedia</p>
+						<p>All content was taken from Wikipedia</p>
+						<p>Code content was taken from the Materialize site</p>
 
 		
 			</article>
