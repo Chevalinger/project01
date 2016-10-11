1fdcfb7eba5501278ab51ac90400572905423422
abarker4 <alizahbarker@Alizahs-MacBook-Pro.local>
2016-10-10 20:04:52 -0400

    deleted commented out code in toppart
    made home button bigger (fixed size)
    
    justified in mobile view but still a spacing issue (some words have a
    lot of space between them in order for the justification to work - not
    sure how it would look on an actual phone)

--- project1/inc/nav.inc.php
+++ project1/inc/nav.inc.php
@@ -3,12 +3,13 @@
       
       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">reorder</i></a>
       <ul id="nav-mobile" class="left hide-on-med-and-down">
-          <li <?php if ($currentPage == "Home") echo 'class="active"';?>><a href="index.php">Home</a></li>
+          <li <?php if ($currentPage == "Home") echo 'class="active"';?>><a class="home-bigger" href="index.php">Home</a></li>
           <li <?php if ($currentPage == "Soccer") echo 'class="active"';?>><a href="soccer.php">Soccer</a></li>
           <li <?php if ($currentPage == "Frisbee") echo 'class="active"';?>><a href="frisbee.php">Frisbee</a></li>
           <li <?php if ($currentPage == "Baseball") echo 'class="active"';?>><a href="baseball.php">Baseball</a></li>
           <li <?php if ($currentPage == "Colophon") echo 'class="active"';?>><a href="colophon.php">Colophon</a></li>
       </ul>
+
        <ul class="side-nav indigo lighten-4" id="mobile-demo">
         <li <?php if ($currentPage == "Home") echo 'class="active"';?>><a href="index.php">Home</a></li>
           <li <?php if ($currentPage == "Soccer") echo 'class="active"';?>><a href="soccer.php">Soccer</a></li>
